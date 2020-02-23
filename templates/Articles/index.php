d6e152be0603196243c9264e8c2a6e7a42fb1390
superterminal <trendemil@gmail.com>
2020-02-23 21:56:13 +0200

    added delete button in index template

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -16,6 +16,10 @@
         </td>
         <td>
             <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+            <?= $this->Form->postLink('Delete', 
+                ['action' => 'delete', $article->slug],
+                ['confirm' => 'Are you sure?'])
+            ?>
         </td>
     </tr>
     <?php endforeach; ?>
