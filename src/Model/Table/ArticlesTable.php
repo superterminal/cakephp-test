0e7e00fbd3611e6211de9862a1a9417300b3639b
superterminal <trendemil@gmail.com>
2020-02-23 21:56:00 +0200

    added validation for articles

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -4,6 +4,7 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table 
 {
@@ -18,5 +19,18 @@ class ArticlesTable extends Table
             $sluggedTitle = Text::slug($entity->title);
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->allowEmptyString('title', false)
+            ->minLength('title', 5)
+            ->maxLength('title', 255)
+
+            ->allowEmptyString('body', false)
+            ->minLength('body', 5);
+
+        return $validator;
     }
 }
