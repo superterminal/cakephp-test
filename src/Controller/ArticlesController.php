10a5a8b795e1f282dbc29f0d8458453385d1c7d0
superterminal <trendemil@gmail.com>
2020-02-23 18:52:04 +0200

    added add method

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,6 +4,14 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
     public function index()
     {
         $this->loadComponent('Paginator');
@@ -15,5 +23,23 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved!'));
+                return $this->redirect(['action' => 'index']);
+            }
+
+            $this->Flash->error(__('Unable to add your article'));
+        }
+        $this->set('article', $article);
     }
 }
